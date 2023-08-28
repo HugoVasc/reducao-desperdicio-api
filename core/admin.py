b0f0263db5c940a34c8e1f2017c0246cbdc28599
RicardoAccorsi <ric.accorsi@gmail.com>
2023-08-28 19:19:04 -0300

    criação da tabela feiras
    criação da tabela feiras e inicialização de arquivos de media e static. Também aproveitei para criar um superuser e mudar as configurações para pt-br e timezone local. Busquei validadores de cep, mas não funcionaram. Criei minha própria função, mas ela não apareceu no admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Feira
 
-# Register your models here.
+# Register your models here. (registrar tabelas do db para poderem ser acessadas pelo admin)
+admin.site.register(Feira)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from .forms import CEPField
 
-# Create your models here.
+# tipos possíveis de feiras
+LISTA_TIPOS = (
+    ("organico", "Orgânico"),
+    ("nao organico", "Não Orgânico")
+)
+
+LISTA_ESTADOS = (
+    ("df", "Distrito Federal"),
+    ("go", "Goiás"),
+    ("mg", "Minas Gerais"),
+)
+
+# Create your models here (tabelas do banco de dados)
+
+# tabela feiras
+class Feira(models.Model):
+    nome = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=15, choices=LISTA_TIPOS)
+    img = models.ImageField(upload_to="img_feiras", default="media/img_feiras/default.jpg")
+    #cep = CEPField() -> não aparece no admin
+    rua = models.CharField(max_length=25)
+    complemento = models.CharField(max_length=25)
+    bairro = models.CharField(max_length=15)
+    estado = models.CharField(max_length=15, choices=LISTA_ESTADOS)
+    
+    def __str__(self):
+        return self.nome
+    
+    
